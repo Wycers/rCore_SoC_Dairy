afcffd08304be21fa5a6c03201d209d13aee2e73
Wycer <wycers@gmail.com>
2020-06-29 00:42:22 +0800

    Learning  example

---
+++ code/step0-learning/Primitives/primitives/src/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+    // Variables can be type annotated.
+    let logical: bool = true;
+
+    let a_float: f64 = 1.0; // Regular annotation
+    let an_integer = 5i32; // Suffix annotation
+
+    // Or a default will be used.
+    let default_float = 3.0; // `f64`
+    let default_integer = 7; // `i32`
+
+    // A type can also be inferred from context
+    let mut inferred_type = 12; // Type i64 is inferred from another line
+    inferred_type = 4294967296i64;
+
+    // A mutable variable's value can be changed.
+    let mut mutable = 12; // Mutable `i32`
+    mutable = 21;
+
+    // Error! The type of a variable can't be changed.
+    // mutable = true;
+
+    // Variables can be overwritten with shadowing.
+    let mutable = true;
+    // let ☢️ = 1;
+    // let 中文 = 2;
+    // let ☢ = 3;
+    // println!("{0}", 中文);
+
+    let test: i32 = 1;
+
+    println!("{0} {1} test: {2}", logical, mutable, test);
+
+    let test = true;
+
+    println!("{0} {1} test: {2}", logical, mutable, test);
+}
