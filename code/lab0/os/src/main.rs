8331ddc3be5c69d60c11a1ad5b907bbd8c04f8d2
Wycer <wycers@gmail.com>
2020-07-05 00:49:36 +0800

    lab0 start

---
+++ code/lab0/os/src/main.rs
@@ -0,0 +1,24 @@
+//! # 全局属性
+//! - `#![no_std]`
+//!   禁用标准库
+#![no_std]
+//!
+//! - `#![no_main]`
+//!   不使用 `main` 函数等全部 Rust-level 入口点来作为程序入口
+#![no_main]
+
+use core::panic::PanicInfo;
+
+/// 当 panic 发生时会调用该函数
+/// 我们暂时将它的实现为一个死循环
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+/// 覆盖 crt0 中的 _start 函数
+/// 我们暂时将它的实现为一个死循环
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
