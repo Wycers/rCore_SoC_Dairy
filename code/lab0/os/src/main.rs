dbb6d5feecee65ff01b1c06de7db25bf3a873128
Wycer <wycers@gmail.com>
2020-07-05 16:22:27 +0800

    lab0

--- code/lab0/os/src/main.rs
+++ code/lab0/os/src/main.rs
@@ -6,6 +6,18 @@
 //! - `#![no_main]`
 //!   不使用 `main` 函数等全部 Rust-level 入口点来作为程序入口
 #![no_main]
+//! # 一些 unstable 的功能需要在 crate 层级声明后才可以使用
+//! - `#![feature(asm)]`
+//!   内嵌汇编
+#![feature(asm)]
+//!
+//! - `#![feature(global_asm)]`
+//!   内嵌整个汇编文件
+#![feature(global_asm)]
+#![feature(llvm_asm)]
+
+// 汇编编写的程序入口，具体见该文件
+global_asm!(include_str!("asm/entry.asm"));
 
 use core::panic::PanicInfo;
 
@@ -16,9 +28,32 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-/// 覆盖 crt0 中的 _start 函数
-/// 我们暂时将它的实现为一个死循环
+/// 在屏幕上输出一个字符，目前我们先不用了解其实现原理
+pub fn console_putchar(ch: u8) {
+    let _ret: usize;
+    let arg0: usize = ch as usize;
+    let arg1: usize = 0;
+    let arg2: usize = 0;
+    let which: usize = 1;
+    unsafe {
+        llvm_asm!("ecall"
+             : "={x10}" (_ret)
+             : "{x10}" (arg0), "{x11}" (arg1), "{x12}" (arg2), "{x17}" (which)
+             : "memory"
+             : "volatile"
+        );
+    }
+}
+
+/// Rust 的入口函数
+///
+/// 在 `_start` 为我们进行了一系列准备之后，这是第一个被调用的 Rust 函数
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn rust_main() -> ! {
+    // 在屏幕上输出 "OK\n" ，随后进入死循环
+    console_putchar(b'O');
+    console_putchar(b'K');
+    console_putchar(b'\n');
+
     loop {}
 }
