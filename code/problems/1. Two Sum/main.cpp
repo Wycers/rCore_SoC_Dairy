7201a83be0ee5e80fdef64ff52a8e8d2d7d8091e
yechangwu <yechangwu@tencent.com>
2020-07-06 21:22:06 +0800

    来刷点lc

---
+++ code/problems/1. Two Sum/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    map<int, int> mp;
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        mp.clear();
+        vector<int> res;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            int need = target - nums[i];
+            map<int, int>::iterator iter = mp.find(need);
+            if (iter != mp.end())
+            {
+                res.push_back(mp[iter->first]);
+                res.push_back(i);
+                break;
+            }
+            mp[nums[i]] = i;
+        }
+        return res;
+    }
+};
+Solution sl;
+int main()
+{
+    vector<int> test = {3, 2, 4};
+    vector<int> res = sl.twoSum(test, 6);
+    printf("%d %d\n", res[0], res[1]);
+    return 0;
+}
