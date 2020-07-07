addf1dd5293e6e7f8f88a48501bb183384621daf
Wycers <wycers@gmail.com>
2020-07-07 20:21:39 +0800

    2020-7-7

---
+++ code/problems/2. Add Two Numbers/solution_rust/src/main.rs
@@ -0,0 +1,90 @@
+/**
+ * [2] Add Two Numbers
+ *
+ * You are given two non-empty linked lists representing two non-negative
+ * integers. The digits are stored in reverse order and each of their nodes
+ * contain a single digit. Add the two numbers and return it as a linked list.
+ *
+ * You may assume the two numbers do not contain any leading zero, except the
+ * number 0 itself.
+ *
+ * Example:
+ *
+ *
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ *
+ */
+pub struct Solution {}
+
+// problem: https://leetcode-cn.com/problems/two-sum/
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+// submission codes start here
+
+impl Solution {
+    pub fn add_two_numbers(
+        l1: Option<Box<ListNode>>,
+        l2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        let mut p = Some(Box::new(ListNode::new(0)));
+
+        p
+    }
+}
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_2() {
+        assert_eq!(
+            Solution::add_two_numbers(to_list(vec![0]), to_list(vec![0])),
+            to_list(vec![0])
+        );
+
+        // assert_eq!(
+        //     Solution::add_two_numbers(to_list(vec![2, 4, 3]), to_list(vec![5, 6, 4])),
+        //     to_list(vec![7, 0, 8])
+        // );
+
+        // assert_eq!(
+        //     Solution::add_two_numbers(to_list(vec![9, 9, 9, 9]), to_list(vec![9, 9, 9, 9, 9, 9])),
+        //     to_list(vec![8, 9, 9, 9, 0, 0, 1])
+        // );
+
+        // assert_eq!(
+        //     Solution::add_two_numbers(to_list(vec![0]), to_list(vec![0])),
+        //     to_list(vec![0])
+        // )
+    }
+}
+
+// helper function for test
+pub fn to_list(vec: Vec<i32>) -> Option<Box<ListNode>> {
+    let mut current = None;
+    for &v in vec.iter().rev() {
+        let mut node = ListNode::new(v);
+        node.next = current;
+        current = Some(Box::new(node));
+    }
+    current
+}
+
+
+fn main() {}
