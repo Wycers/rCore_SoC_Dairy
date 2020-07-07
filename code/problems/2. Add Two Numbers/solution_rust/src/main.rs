18ee7bd21c051a275fa7d04b280d76c932e1784c
Wycer <wycers@gmail.com>
2020-07-07 23:42:12 +0800

    2020-7-7

--- code/problems/2. Add Two Numbers/solution_rust/src/main.rs
+++ code/problems/2. Add Two Numbers/solution_rust/src/main.rs
@@ -40,9 +40,41 @@ impl Solution {
         l1: Option<Box<ListNode>>,
         l2: Option<Box<ListNode>>,
     ) -> Option<Box<ListNode>> {
-        let mut p = Some(Box::new(ListNode::new(0)));
+        let mut res = Some(Box::new(ListNode::new(0)));
+        let mut p = &mut res;
 
-        p
+        let (mut l1, mut l2) = (l1, l2);
+
+        let mut c = 0;
+
+        loop {
+            if let Some(v1) = l1 {
+                c += v1.val;
+                l1 = v1.next;
+            }
+            if let Some(v2) = l2 {
+                c += v2.val;
+                l2 = v2.next;
+            }
+
+            let flag = l1 == None && l2 == None;
+
+            if let Some(i) = p {
+                i.val = c % 10;
+                c /= 10;
+                i.next = if flag && c == 0 {
+                    None
+                } else {
+                    Some(Box::new(ListNode::new(c)))
+                };
+                p = &mut i.next;
+            }
+
+            if flag {
+                break;
+            }
+        }
+        res
     }
 }
 // submission codes end
@@ -54,24 +86,19 @@ mod tests {
     #[test]
     fn test_2() {
         assert_eq!(
-            Solution::add_two_numbers(to_list(vec![0]), to_list(vec![0])),
-            to_list(vec![0])
+            Solution::add_two_numbers(to_list(vec![2, 4, 3]), to_list(vec![5, 6, 4])),
+            to_list(vec![7, 0, 8])
         );
 
-        // assert_eq!(
-        //     Solution::add_two_numbers(to_list(vec![2, 4, 3]), to_list(vec![5, 6, 4])),
-        //     to_list(vec![7, 0, 8])
-        // );
+        assert_eq!(
+            Solution::add_two_numbers(to_list(vec![9, 9, 9, 9]), to_list(vec![9, 9, 9, 9, 9, 9])),
+            to_list(vec![8, 9, 9, 9, 0, 0, 1])
+        );
 
-        // assert_eq!(
-        //     Solution::add_two_numbers(to_list(vec![9, 9, 9, 9]), to_list(vec![9, 9, 9, 9, 9, 9])),
-        //     to_list(vec![8, 9, 9, 9, 0, 0, 1])
-        // );
-
-        // assert_eq!(
-        //     Solution::add_two_numbers(to_list(vec![0]), to_list(vec![0])),
-        //     to_list(vec![0])
-        // )
+        assert_eq!(
+            Solution::add_two_numbers(to_list(vec![0]), to_list(vec![0])),
+            to_list(vec![0])
+        )
     }
 }
 
@@ -85,6 +112,5 @@ pub fn to_list(vec: Vec<i32>) -> Option<Box<ListNode>> {
     }
     current
 }
-
 
 fn main() {}
