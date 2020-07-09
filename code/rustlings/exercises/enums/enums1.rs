01dc6c586cfb6fa0aed8ed73f2a3920d2f831a3b
wycer <wycers@gmail.com>
2020-07-09 16:04:25 +0800

    rustlings enums

--- code/rustlings/exercises/enums/enums1.rs
+++ code/rustlings/exercises/enums/enums1.rs
@@ -1,11 +1,13 @@
 // enums1.rs
 // Make me compile! Execute `rustlings hint enums1` for hints!
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
     // TODO: define a few types of messages as used below
+    Echo,
+    Quit,
+    Move,
+    ChangeColor
 }
 
 fn main() {
